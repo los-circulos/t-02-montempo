9a0472f1e37c1cf3413c7a480f7f1605cce18efb
los Circulos <correos.los.circulos@gmail.com>
2022-10-22 02:50:02 +0200

    test screen write with u8x8

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,25 @@
 #include "config.h"
 #include "hardware.h"
 #include "main.h"
-//#include "U8g2/src/U8x8lib.h"
+#include <U8x8lib.h>
 
-//U8X8_SSD1306_128X32_UNIVISION_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);   // Adafruit ESP8266/32u4/ARM Boards + FeatherWing OLED
+U8X8_SSD1306_128X32_UNIVISION_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);   // Adafruit ESP8266/32u4/ARM Boards + FeatherWing OLED
 
 bool asd = true;
 
 void setup() {
 // write your initialization code here
     readConfig();
-//    u8x8.begin();
-//    u8x8.setPowerSave(0);
+    u8x8.begin();
+    u8x8.setPowerSave(0);
+
+    // @todo display logo, version
+
+    // show current settings
+    u8x8.clear();
+    u8x8.setFont(u8x8_font_amstrad_cpc_extended_f);
+    u8x8.drawString(0, 0, "0123456789");
+    u8x8.drawString(0, 1, "0123456789");
 
 }
 
