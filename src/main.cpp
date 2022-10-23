74b7639ced553d41c38b0aa53519d3a95c047515
los Circulos <correos.los.circulos@gmail.com>
2022-10-23 21:06:09 +0200

    initial screens

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,16 @@
 #include "config.h"
 #include "hardware.h"
 #include "main.h"
-#include <U8x8lib.h>
+#include <U8g2lib.h>
+#include <Wire.h>
+#include <screen.h>
+//#include <U8x8lib.h>
 
+//public: U8X8_SSD1306_128X32_UNIVISION_HW_I2C(uint8_t reset = U8X8_PIN_NONE, uint8_t clock = U8X8_PIN_NONE, uint8_t data = U8X8_PIN_NONE) : U8X8() {
 U8X8_SSD1306_128X32_UNIVISION_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);   // Adafruit ESP8266/32u4/ARM Boards + FeatherWing OLED
+
+// for u8g2 this works
+//U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0);   // Adafruit ESP8266/32u4/ARM Boards + FeatherWing OLED
 
 bool asd = true;
 
@@ -18,13 +25,45 @@ void setup() {
 
     // show current settings
     u8x8.clear();
-    u8x8.setFont(u8x8_font_amstrad_cpc_extended_f);
-    u8x8.drawString(0, 0, "0123456789");
-    u8x8.drawString(0, 1, "0123456789");
+    u8x8.setFont(FONT_S);
+    u8x8.drawString(0, 3, "F2B");
+    u8x8.drawString(11, 3, "V1.0");
+    u8x8.setFont(FONT_XL);
+    u8x8.drawString(0, 0, "MONTEMPO");
+    delay(2000);
+
+    u8x8.clear();
+
+    if (config.testMode) {
+        u8x8.drawString(0, 0, "TEST");
+    }
+    else {
+        u8x8.drawString(0, 0, "FLY?");
+    }
+    u8x8.setFont(FONT_S);
+//    if (config.holdRPM || true) {
+    if (config.holdRPM) {
+        u8x8.drawString(0, 3, "RPM 8.5");
+    }
+//    else if (config.holdCurrent || true) {
+    else if (config.holdCurrent) {
+        u8x8.drawString(0, 3, "NOTYET:(");
+    }
+    else {
+        u8x8.drawString(0, 3, "THR  82%");
+    }
+
+    u8x8.setFont(FONT_L);
+    u8x8.drawString(10,0, "12.4 V");
+    u8x8.drawString(11, 2, "10:10");
 
 }
 
 void loop() {
+
+//    readConfig();
+//    delay(100);
+
 // write your code here
 //    u8x8.clear();
 ////  u8x8.setFont(u8x8_font_chroma48medium8_r);
---
+++ src/screen.h
@@ -0,0 +1,9 @@
+#ifndef MONTEMPO_SCREEN_H
+#define MONTEMPO_SCREEN_H
+
+#define FONT_S u8x8_font_amstrad_cpc_extended_u
+//#define FONT_S u8x8_font_amstrad_cpc_extended_r
+#define FONT_L u8x8_font_8x13B_1x2_f
+#define FONT_XL u8x8_font_profont29_2x3_r
+
+#endif //MONTEMPO_SCREEN_H
